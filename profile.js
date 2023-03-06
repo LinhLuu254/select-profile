f67b8ffefae17c2b8d8439eefeca6c4504c32e85
LinhLuu254 <llinh254@gmail.com>
2023-03-06 15:37:58 -0500

    jquery

---
+++ profile.js
@@ -0,0 +1,12 @@
+
+const setHeading = () =>{
+
+    $("#heading1").text("Linh Luu");
+    $("#heading2").text("CIT");
+    $("#heading3").text("Senior");
+
+    $('#heading4').html("<em>Vietnam</em>");
+}
+
+$(document).ready(setHeading);
+
